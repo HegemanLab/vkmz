82722e42168a949394c81a58dc84737eb2148cd7
Mark Esler <eslerm@umn.edu>
2017-09-29 12:04:41 -0500

    updated input mzXML parser

--- aggregateScratch.py
+++ aggregateScratch.py
@@ -11,10 +11,11 @@ from flexPlot import plotVanK
 parser = argparse.ArgumentParser()
 parser.add_argument('--load',      '-l', nargs='?', default='',     help='Load a previously generated ratio table. Set file path. Disabled by default.')
 parser.add_argument('--input',     '-i', nargs='*', default='',     help='Enter full mzXML/mzML file paths. For multiple files seperate files with a space.')
-parser.add_argument('--output',    '-o', action="store_true", default='False', help='Call variable to ouput a ratio file.')
+parser.add_argument('--output',    '-o', action='store_true', default='False', help='Call variable to ouput a ratio file.')
 parser.add_argument('--threshold', '-t', nargs='?', default='10',   type=int, help='Set threshold as a percent integer from 0 to 100. eg. 15 for 15%%.')
-parser.add_argument('--polarity',  '-p', nargs='?', default='both', type=str, help='Set to "pos", "neg", or "both". Default is "both".')
-parser.add_argument('--plottype', '-pt', nargs='*', default='[scatter]', help='Set to "scatter", "heatmap", or "3d". Default is "scatter".')
+parser.add_argument('--polarity',  '-p', nargs='?', default='both', type=str, choices=['both', 'pos', 'neg'], help='Set to "pos", "neg", or "both". Default is "both". Only one plot type can be set.')
+parser.add_argument('--plottype', '-pt', nargs='*', default=['scatter'], choices=['scatter', 'heatmap', '3d'], help='Set to "scatter", "heatmap", or "3d". Default is "scatter".')
+parser.add_argument('--multiprocessing', '-m', action='store_true', default='False', help='Call variable to use multiprocessing. One process per core.')
 args = parser.parse_args()
 
 # exits script if a load argument is used
@@ -50,6 +51,19 @@ else:
       raise ValueError('Input was set to "%s". It must be set to the filepath of a mzML or mzXML file.' % (vkFile))
       exit()
 
+# Use correct processing based on file type. Gets both pos and neg mode because this check is made anyways s o little
+# cost to just gather both sets here.
+for vkFile in vkInputFiles:
+  if vkFile.lower().endswith('.mzxml'):
+      mzXML = MzXML()
+      mzXML.parse_file(vkFile)
+      vkInputMzs = XML_process(mzXML, threshold=vkThreshold)
+  elif vkFile.lower().endswith('.mzxml'):
+      vkInputMzs = ML_process(f, threshold=vkThreshold)
+# Removes all duplicates from both neg and pos lists
+vkInputMzs[0] = list(set(vkInputMzs[0]))
+vkInputMzs[1] = list(set(vkInputMzs[1]))
+
 # set output boolean
 vkOutput = getattr(args, "output")
 if vkOutput == True:
@@ -69,14 +83,28 @@ else:
   exit()
 
 # read polarity
+# argparse sanitizes input
 vkPolarity = getattr(args, 'polarity').lower()
-if vkPolarity == 'positive' or vkPolarity == '+':
-  vKPolarity = 'pos'
-elif vkPolarity == 'negative' or vkPolarity == '-':
-  vKPolarity = 'neg'
-if vkPolarity != 'both' and vkPolarity != 'pos' and vkPolarity != 'neg':
-  raise ValueError('Polarity was set to "%s". It must be set to "both", "pos", or "neg".' % (vkPolarity))
-  exit()
+
+# vk ratio dataset builder for each polarity
+# dataset is list with four elements.
+# input mass to charge ratios checked against database of masses with known chemical structure
+# each identified mass is saved across the four elements, at a given index, of the dataset list
+# the first  element represents ratio of hydrogens in identified masses structure
+# the second element represents ratio of carbons                    #FLAG VERIRFY
+# the third  element represents if a nitrogen is in the strcutre    #FLAG remove element, have plotter check last element != 0
+# the fourth element represents ratio of nitrogens
+# set --output to creates ratio csv files
+def buildRatios(vkPolarity):
+  if vkPolarity == 'both':
+    buildRatios('pos') 
+    buildRatios('neg') 
+  if vkPolarity == 'pos':
+    print('pos ratio')
+  if vkPolarity == 'neg':
+    print('neg ratio')
+
+buildRatios(vkPolarity)
 
 # read plot-type
 vkPlotType = getattr(args, 'plottype')
@@ -92,65 +120,60 @@ else:
     else:
       raise ValueError('Input was set to "%s". It must be set to the filepath of a mzML or mzXML file.' % (vkFile))
       exit()
+  for vkType in vkPlotTypes:
+    print(vkType)
 
-# prepare to process mzs from input files
-neg_pos_mz_sets = [[], []]
-
-# Use correct processing based on file type. Gets both pos and neg mode because this check is made anyways s o little
-# cost to just gather both sets here.
-for vkFile in vkInputFiles:
-  if vkFile.lower().endswith('.mzxml'):
-      mzXML = MzXML()
-      mzXML.parse_file(vkFile)
-      neg_pos_mz_sets_temp = XML_process(mzXML, threshold=vkThreshold)
-      neg_pos_mz_sets[0] = neg_pos_mz_sets[0] + neg_pos_mz_sets_temp[0]
-      neg_pos_mz_sets[1] = neg_pos_mz_sets[1] + neg_pos_mz_sets_temp[1]
-  elif vkFile.lower().endswith('.mzxml'):
-      # Use ML processing
-      neg_pos_mz_sets_temp = ML_process(f, threshold=vkThreshold)
-      neg_pos_mz_sets[0] = neg_pos_mz_sets[0] + neg_pos_mz_sets_temp[0]
-      neg_pos_mz_sets[1] = neg_pos_mz_sets[1] + neg_pos_mz_sets_temp[1]
-# Removes all duplicates from both neg and pos lists
-neg_pos_mz_sets[0] = list(set(neg_pos_mz_sets[0]))
-neg_pos_mz_sets[1] = list(set(neg_pos_mz_sets[1]))
-
-mode = vkPolarity
+vkMultiprocessing = getattr(args, "multiprocessing")
+if vkMultiprocessing:
+  import multiprocessing
+  vkCores = multiprocessing.cpu_count()
+  print(vkCores)
 
 # get lookup table.
 lt = bmrb.getLookupTable('bmrb-db2.csv')
-
 # set up. elements could be changed but would need to do some editing elsewhere.
 elements = ['C', 'H', 'O', 'N']
 neg_comps = list()
 pos_comps = list()
 
+print('set fin')
+
+def vkPlotter(vkPolarity):
+  return
+
 # check to see if user wanted to process neg mode scans
-if mode == 'neg' or mode == 'both':
-  for mz in neg_pos_mz_sets[0]:
+if vkPolarity == 'neg' or vkPolarity == 'both':
+  for mz in vkInputMzs[0]:
     neg_comps.append(bmrb.getFormulaFromMass(bmrb.adjust(mz, 'neg'), lt, tolerance=error)) # adjust mass and search in lookup table. Store result in list.
   neg_comps = filter(lambda a: a != 'No Match', neg_comps) # Filter out no matches
   neg_elements = extractNeededElementalData.find_elements_values(elements_to_find=elements, compounds=neg_comps) # Get elements from compounds
   neg_ratios = processElementalData.process_elemental_data(neg_elements) # Turn elements into ratios
   if write_ratios == True: 
     neg_filename = 'example-ratios-neg.csv'  # forcing file name. FLAG
-    with open(neg_filename, mode='w') as f: 
+    with open(neg_filename, 'w') as f: 
       for ratio in neg_ratios:
         f.writelines(str(ratio).strip('[]') + '\n')
   plotType = vkPlotTypes
   for vkType in vkPlotTypes:
     print(vkType)
     plotVanK(ratiosList=neg_ratios, typeOfPlot=vkType)
-
-if mode == 'pos' or mode == 'both':
-  for mz in neg_pos_mz_sets[1]:
+if vkPolarity == 'pos' or vkPolarity == 'both':
+  for mz in vkInputMzs[1]:
     pos_comps.append(bmrb.getFormulaFromMass(bmrb.adjust(mz, 'pos'), lt)) # adjust mass and search in lookup table. Store result in list.
   pos_comps = filter(lambda a: a != 'No Match', pos_comps) # Filter out no matches
+  print("pos_comps is")
+  print(pos_comps)
   pos_elements = extractNeededElementalData.find_elements_values(elements_to_find=elements, compounds=pos_comps) # Get elements from compounds
+  print("pos_elements is")
+  print(pos_elements)
   pos_ratios = processElementalData.process_elemental_data(pos_elements) # Turn elements into ratios
-  if write_ratios == True: 
+  print("pos_ratios is")
+  print(pos_ratios)
+  if write_ratios == True:
     pos_filename = 'example-ratios-pos.csv'  # forcing file name. FLAG
-    with open(neg_filename, mode='w') as f: 
+    with open(neg_filename, 'w') as f: 
       for ratio in neg_ratios:
+        print(ratio)
         f.writelines(str(ratio).strip('[]') + '\n')
   plotType = vkPlotTypes
   for vkType in vkPlotTypes:
