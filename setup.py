08a0f9cd2d311f7dee2d38821cb19e03638c7579
eslerm <eslerm@umn.edu>
2018-07-16 21:35:24 +0000

    inital conda build

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+setup.py
+~~~~~~~~~~~~
+:copyright: (c) 2018 Hegeman Lab, University of Minnesota
+:license: MIT
+"""
+
+from setuptools import setup
+
+setup(
+    name="VKMZ",
+    version="1.3.0",
+    description="metabolomics formula prediction and van Krevelen diagram generation",
+    author="Mark Esler",
+    author_email="eslerm@umn.edu",
+    url="https://github.com/HegemanLab/VKMZ",
+    license="MIT",
+    packages=["vkmz"]
+)
